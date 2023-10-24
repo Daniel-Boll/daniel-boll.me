23653131748ba91c446dc56d55c344584fe8afc5
Daniel Boll <danielboll.academico@gmail.com>
2023-10-23 23:36:02 -0300

    feat(posts/[slug].astro): add support for retrieving assets path in open graph image path fix(open-graph/image-path.ts): add function to retrieve assets path from filename
    The changes in `posts/[slug].astro` include importing the `getAssetsPath` function from `open-graph/image-path.ts` and using it to generate the image path for the open graph metadata. This allows for more flexibility in specifying the image path.
    
    In `open-graph/image-path.ts`, a new function `getAssetsPath` is added to retrieve the assets path from a given filename. This function is then used in the `getOgImagePath` function to generate the open graph image path. This change improves the modularity and reusability of the code.

--- src/utils/open-graph/image-path.ts
+++ src/utils/open-graph/image-path.ts
@@ -1,3 +1,7 @@
+export const getAssetsPath = (filename: string = "Default Value") => {
+  return `/assets/${filename.split("/assets/")[1]}`;
+};
+
 export const getOgImagePath = (filename: string = "Default Value") => {
   if (filename.startsWith("/")) filename = filename.substring(1);
 
@@ -8,4 +12,3 @@ export const getOgImagePath = (filename: string = "Default Value") => {
 
   return `./open-graph/${filename}.png`;
 };
-
