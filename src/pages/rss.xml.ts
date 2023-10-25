be57811f3b45906284020f882c3f6074c643ed76
Daniel Boll <danielboll.academico@gmail.com>
2023-10-25 10:41:45 -0300

    feat(rss): add rss feed
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export const GET: APIRoute = async ({ site }) => {
+  const blog = await getCollection("posts");
+
+  return rss({
+    title: "Daniel Boll’s Blog",
+    description:
+      "Salve! I'm a Tech Lead from Brazil. I'm passionate about technology and constantly seeking new challenges to expand my skillset. I enjoy mastering new programming languages and frameworks and contributing to open source projects. I also like sharing my progress through live coding.",
+    site: site ?? "https://daniel-boll.me",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.publishedAt,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    stylesheet: "/pretty-feed-v3.xsl",
+  });
+};
