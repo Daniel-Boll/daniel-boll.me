6bd724c8f39bec58bf645c559b3301d2921131e6
Daniel Boll <danielboll.academico@gmail.com>
2025-04-13 15:23:57 -0300

    feat(favicon): add favicon
    and friend list
    
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -31,11 +31,11 @@ export const siteConfig: SiteConfig = {
   dynamicOgImage: true,
   favicon: [
     // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: "/favicon/favicon.svg", // Path of the favicon, relative to the /public directory
+      // theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      // sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
   ],
 };
 
@@ -44,12 +44,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    // TODO: Perhaps add a CV here
-    // {
-    //   name: "GitHub",
-    //   url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-    //   external: true, // Show an external link icon and will open in a new tab
-    // },
+    {
+      name: "CV",
+      url: "/daniel-boll-cv.pdf",
+      external: true,
+    },
   ],
 };
 
